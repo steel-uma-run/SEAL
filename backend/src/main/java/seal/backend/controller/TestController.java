ce3fc9e1377745a279ce75a7207a858160462255
mynaux171 <thmyxuan11@gmail.com>
2026-05-28 16:06:05 +0700

    Test: Thêm API Hello World để kiểm tra luồng Pull Request

---
+++ backend/src/main/java/seal/backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package seal.backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/hello")
+    public String sayHello() {
+        return "Hello!🎉";
+    }
+}
