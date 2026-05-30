2fd3b37d3e5024b4ee4cd7a03617773c66e83806
Scarborrower <ng.k.dien123@gmail.com>
2026-05-30 20:58:52 +0700

    add track entity

---
+++ backend/src/main/java/seal/backend/entities/Track.java
@@ -0,0 +1,58 @@
+package seal.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "tracks")
+public class Track {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private java.util.UUID id;
+
+  @Column(columnDefinition = "timestamptz", nullable = false)
+  private OffsetDateTime startTime;
+
+  @Column(columnDefinition = "timestamptz", nullable = false)
+  private OffsetDateTime endTime;
+
+  @Column(columnDefinition = "TEXT", nullable = true)
+  private String description;
+
+  public java.util.UUID getId() {
+    return id;
+  }
+
+  public void setId(java.util.UUID id) {
+    this.id = id;
+  }
+
+  public OffsetDateTime getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(OffsetDateTime startTime) {
+    this.startTime = startTime;
+  }
+
+  public OffsetDateTime getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(OffsetDateTime endTime) {
+    this.endTime = endTime;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
