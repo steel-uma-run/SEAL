87ff49e17da36d1c8a4a70683ae8d8edb6f733a0
Scarborrower <ng.k.dien123@gmail.com>
2026-05-30 20:53:00 +0700

    add getters and setters for team entity

--- backend/src/main/java/seal/backend/entities/Team.java
+++ backend/src/main/java/seal/backend/entities/Team.java
@@ -25,4 +25,36 @@ public class Team {
 
   @Enumerated(EnumType.STRING)
   private TeamStatus teamStatus;
+
+  public java.util.UUID getId() {
+    return id;
+  }
+
+  public void setId(java.util.UUID id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public TeamStatus getTeamStatus() {
+    return teamStatus;
+  }
+
+  public void setTeamStatus(TeamStatus teamStatus) {
+    this.teamStatus = teamStatus;
+  }
 }
