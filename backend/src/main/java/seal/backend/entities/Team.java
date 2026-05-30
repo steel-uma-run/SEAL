28d8cd2f079cf998cfd9db4e947e15b0fcebaa97
Scarborrower <ng.k.dien123@gmail.com>
2026-05-30 20:51:33 +0700

    add team entity

---
+++ backend/src/main/java/seal/backend/entities/Team.java
@@ -0,0 +1,28 @@
+package seal.backend.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import seal.backend.enums.TeamStatus;
+
+@Entity
+@Table(name = "teams")
+public class Team {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private java.util.UUID id;
+
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String name;
+
+  @Column(nullable = true, columnDefinition = "TEXT")
+  private String description;
+
+  @Enumerated(EnumType.STRING)
+  private TeamStatus teamStatus;
+}
