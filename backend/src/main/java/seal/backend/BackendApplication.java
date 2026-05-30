2f5074e0838e6ce9412e67814f3072c5bd87cfc1
ntt <nttis@duck.com>
2026-05-30 19:48:33 +0700

    Picking up the trash

--- backend/src/main/java/seal/backend/BackendApplication.java
+++ backend/src/main/java/seal/backend/BackendApplication.java
@@ -1,23 +1,13 @@
 package seal.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 @RestController
 public class BackendApplication {
-
-	// 2. Gọi công cụ JdbcTemplate để test Database
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
-
 }
--- backend/src/main/java/seal/backend/model/Coordinator.java
+++
@@ -1,9 +0,0 @@
-package seal.backend.model;
-
-import seal.backend.enums.Role;
-
-public class Coordinator extends User {
-    public Coordinator(String email, String password, String fullName) {
-        super(email, password, fullName, Role.COORDINATOR);
-    }
-}
