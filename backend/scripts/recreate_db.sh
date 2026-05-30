b06c2414ae1ae58e668461f4b6afa89082f15ed5
ntt <nttis@duck.com>
2026-05-30 19:48:33 +0700

    Picking up the trash

--- backend/scripts/recreate_db.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-psql --dbname postgres --command "DROP DATABASE seal;"
-psql --dbname postgres --command "CREATE DATABASE seal;"
-
-psql --dbname seal --file ./init.sql
--- backend/src/main/java/seal/backend/model/Sysadmin.java
+++
@@ -1,9 +0,0 @@
-package seal.backend.model;
-
-import seal.backend.enums.Role;
-
-public class Sysadmin extends User {
-    public Sysadmin(String email, String password, String fullName) {
-        super(email, password, fullName, Role.SYSADMIN);
-    }
-}
