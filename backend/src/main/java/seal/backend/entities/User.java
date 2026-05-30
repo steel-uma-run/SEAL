31ba5665e30973a9c6818578d9f8ffe08ef1cbb2
scar <scarborrower@gmail.com>
2026-05-30 19:48:33 +0700

    edit User enitity
    user entities previously has some incorrect constraint and not enough
    field. this commit added the role, student type, status field.

--- backend/src/main/java/seal/backend/entities/User.java
+++ backend/src/main/java/seal/backend/entities/User.java
@@ -2,10 +2,15 @@ package seal.backend.entities;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import seal.backend.enums.Role;
+import seal.backend.enums.StudentStatus;
+import seal.backend.enums.StudentType;
 
 @Entity
 @Table(name = "users")
@@ -17,37 +22,75 @@ public class User {
     @Column(nullable = false, columnDefinition = "TEXT")
     private String fullName;
 
-    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    private StudentType studentType;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String passwordHash;
 
-    public String getPasswordHash() {
-        return passwordHash;
-    }
+    @Enumerated(EnumType.STRING)
+    private StudentStatus studentStatus;
 
-    public void setPasswordHash(String passwordHash) {
-        this.passwordHash = passwordHash;
-    }
+	public java.util.UUID getId() {
+		return id;
+	}
 
-    public java.util.UUID getId() {
-        return id;
-    }
+	public void setId(java.util.UUID id) {
+		this.id = id;
+	}
 
-    public String getFullName() {
-        return fullName;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public Role getRole() {
+		return role;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public StudentType getStudentType() {
+		return studentType;
+	}
+
+	public void setStudentType(StudentType studentType) {
+		this.studentType = studentType;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public StudentStatus getStudentStatus() {
+		return studentStatus;
+	}
+
+	public void setStudentStatus(StudentStatus studentStatus) {
+		this.studentStatus = studentStatus;
+	}
+
 }
