5b9dd3747ff09f6948de9d11b3a434cd62428438
mynaux171 <thmyxuan11@gmail.com>
2026-05-31 15:50:44 +0700

    Rename: Profile -> User || ProfileService -> UserService

--- backend/src/main/java/seal/backend/model/Coordinator.java
+++ backend/src/main/java/seal/backend/model/Coordinator.java
@@ -2,7 +2,7 @@ package seal.backend.model;
 
 import seal.backend.enums.Role;
 
-public class Coordinator extends Profile {
+public class Coordinator extends User {
     public Coordinator(String email, String password, String fullName) {
         super(email, password, fullName, Role.COORDINATOR);
     }
--- backend/src/main/java/seal/backend/model/Judge.java
+++ backend/src/main/java/seal/backend/model/Judge.java
@@ -2,7 +2,7 @@ package seal.backend.model;
 
 import seal.backend.enums.Role;
 
-public class Judge extends Profile {
+public class Judge extends User {
     public Judge(String email, String password, String fullName) {
         super(email, password, fullName, Role.JUDGE);
     }
