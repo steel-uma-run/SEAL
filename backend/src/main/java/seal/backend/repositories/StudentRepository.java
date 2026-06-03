8c82b9fc497a433f7a59dd3d076cee6d0a313b21
mynaux171 <thmyxuan11@gmail.com>
2026-06-04 01:21:04 +0700

    API to fetch unapproved students for coordinator

--- backend/src/main/java/seal/backend/repositories/StudentRepository.java
+++ backend/src/main/java/seal/backend/repositories/StudentRepository.java
@@ -1,3 +1,9 @@
 package seal.backend.repositories;
 
-public interface StudentRepository extends UserRepository {}
+import java.util.List;
+import seal.backend.entities.Student;
+import seal.backend.enums.StudentStatus;
+
+public interface StudentRepository extends UserRepository {
+  List<Student> findByStudentStatus(StudentStatus studentStatus);
+}
