678817f51a0088634f73c127ddf7f8576c196f0b
Scarborrower <ng.k.dien123@gmail.com>
2026-06-03 17:50:38 +0700

    Reformat

--- backend/src/main/java/seal/backend/entities/Coordinator.java
+++ backend/src/main/java/seal/backend/entities/Coordinator.java
@@ -2,16 +2,15 @@ package seal.backend.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-
 import seal.backend.enums.Role;
 
 @Entity
 @Table(name = "coordinators")
 public class Coordinator extends User {
-    public Coordinator() {}
+  public Coordinator() {}
 
-    public Coordinator(User user) {
-        super(user.getFullName(), user.getEmail(), user.getPasswordHash());
-        setRole(Role.COORDINATOR);
-    }
+  public Coordinator(User user) {
+    super(user.getFullName(), user.getEmail(), user.getPasswordHash());
+    setRole(Role.COORDINATOR);
+  }
 }
