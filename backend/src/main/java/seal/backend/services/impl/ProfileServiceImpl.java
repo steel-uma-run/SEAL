a31f8ef324dc09cda4207fe581d64a26954b227a
ntt <nttis@duck.com>
2026-06-03 16:25:10 +0700

    Setup ProfileService

---
+++ backend/src/main/java/seal/backend/services/impl/ProfileServiceImpl.java
@@ -0,0 +1,9 @@
+package seal.backend.services.impl;
+
+import org.springframework.stereotype.Component;
+import seal.backend.services.ProfileService;
+
+@Component
+public class ProfileServiceImpl implements ProfileService {
+  
+}
