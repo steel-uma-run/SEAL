baadcc94fb29b4026e87f7d51450c9d46b65a694
ntt <nttis@duck.com>
2026-06-03 18:11:34 +0700

    Implement account approve API

--- backend/src/main/java/seal/backend/controllers/ProfileController.java
+++ backend/src/main/java/seal/backend/controllers/ProfileController.java
@@ -1,9 +1,16 @@
 package seal.backend.controllers;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import seal.backend.exceptions.StudentNotPendingException;
+import seal.backend.exceptions.UserNotStudentException;
 import seal.backend.responses.ProfileResponse;
 import seal.backend.services.ProfileService;
 
@@ -19,5 +26,18 @@ public class ProfileController {
     ProfileResponse profile = profileService.get(email);
 
     return ResponseEntity.ok(profile);
+  }
+
+  @PreAuthorize("hasAuthority('COORDINATOR')")
+  @PostMapping("/{id}/approve")
+  public ResponseEntity<?> approve(@PathVariable UUID id) {
+    try {
+      profileService.approve(id);
+      return ResponseEntity.ok().build();
+    } catch (UsernameNotFoundException ex) {
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, ex.getMessage());
+    } catch (UserNotStudentException | StudentNotPendingException ex) {
+      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, ex.getMessage());
+    }
   }
 }
--- backend/src/main/java/seal/backend/services/ProfileService.java
+++ backend/src/main/java/seal/backend/services/ProfileService.java
@@ -1,9 +1,16 @@
 package seal.backend.services;
 
+import java.util.UUID;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import seal.backend.exceptions.StudentNotPendingException;
+import seal.backend.exceptions.UserNotStudentException;
 import seal.backend.responses.ProfileResponse;
 
 public interface ProfileService {
   // I think it's reasonable to assume that this will never receive a
   // non-existent email?
   public ProfileResponse get(String email);
+
+  public void approve(UUID id)
+      throws UsernameNotFoundException, UserNotStudentException, StudentNotPendingException;
 }
