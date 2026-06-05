c6cf0019a8b18c24499c3d242bfd6c2f25aec4d0
ntt <nttis@duck.com>
2026-06-05 19:07:40 +0700

    add login page

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<main class="w-screen h-screen"></main>
+
+<style>
+	main {
+		background-color: var(--md-sys-color-surface);
+	}
+</style>
---
+++ frontend/src/routes/login/+page.svelte
@@ -0,0 +1,46 @@
+<main class="w-screen h-screen justify-center content-center">
+	<div class="max-w-md w-full m-auto flex flex-col items-center rounded-md p-4">
+		<h1 class="text-xl font-bold">Welcome back</h1>
+		<p class="mb-6" style="color: var(--md-sys-color-on-surface-variant)">
+			Login with your account
+		</p>
+
+		<form class="w-full flex flex-col gap-6">
+			<label>
+				<p>Email</p>
+				<input class="w-full rounded-md" type="email" placeholder="Enter email" required />
+			</label>
+
+			<label>
+				<span class="flex justify-between">
+					<p>Password</p>
+					<a href="/forgot-password" class="text-sm text-blue-500 underline">Forgot password?</a>
+				</span>
+				<input class="w-full rounded-md" type="password" required />
+			</label>
+
+			<input
+				value="Login"
+				type="submit"
+				style="background-color: var(--md-sys-color-primary-container); color: var(--md-sys-color-on-primary-container)"
+				class="rounded-md p-2 font-semibold hover:cursor-pointer hover:brightness-75 transition-all"
+			/>
+		</form>
+
+		<span class="mt-4"
+			>Don't have an account yet? <a href="/register" class="text-blue-500 underline"
+				>Register now.</a
+			></span
+		>
+	</div>
+</main>
+
+<style>
+	main {
+		background-color: var(--md-sys-color-surface);
+	}
+
+	input {
+		border-color: var(--md-sys-color-outline);
+	}
+</style>
