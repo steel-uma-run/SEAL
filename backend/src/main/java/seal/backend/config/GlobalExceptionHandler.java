dd7ac90c79a59024c11badd1a2a572f30cf35e46
ntt <nttis@duck.com>
2026-06-08 17:39:50 +0700

    customize MethodArgumentNotValidException handler

--- backend/src/main/java/seal/backend/config/GlobalExceptionHandler.java
+++ backend/src/main/java/seal/backend/config/GlobalExceptionHandler.java
@@ -1,9 +1,18 @@
 package seal.backend.config;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @RestControllerAdvice
@@ -14,5 +23,27 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
 
     return problemDetail;
+  }
+
+  // Custom MethodArgumentNotValidException handler to return actual, real, readable error messages
+  // instead of just "Invalid request content."
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(
+      MethodArgumentNotValidException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    List<FieldError> errors = ex.getFieldErrors();
+    Map<String, Object> errorDetails = new HashMap<>();
+
+    for (FieldError err : errors) {
+      errorDetails.put(err.getField(), err.getDefaultMessage());
+    }
+
+    ProblemDetail body = ex.getBody();
+    body.setStatus(HttpStatus.UNPROCESSABLE_CONTENT);
+    body.setProperty("errors", errorDetails);
+
+    return ResponseEntity.unprocessableContent().body(body);
   }
 }
