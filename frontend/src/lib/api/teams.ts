2666a765bcd6a7c998b7b4cfe70ac83fa306e7c4
NTT7903 <165687643+NTT7903@users.noreply.github.com>
2026-06-09 18:23:49 +0700

    create dashboard and api for each function

---
+++ frontend/src/lib/api/teams.ts
@@ -0,0 +1,18 @@
+import { API_BASE } from "."
+import { getToken } from "./auth"
+
+export async function createTeam(teamName: string, description: string, seasonId: string, leaderId: string) {
+    return fetch(`${API_BASE}/teams`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${getToken()}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            name: teamName,
+            description: description,
+            seasonId: seasonId,
+            leaderId: leaderId
+        })
+    })
+}
