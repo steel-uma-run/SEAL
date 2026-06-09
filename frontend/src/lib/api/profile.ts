5373d8ceef253435be17a98b6db726f193390a11
NTT7903 <165687643+NTT7903@users.noreply.github.com>
2026-06-09 18:23:49 +0700

    create dashboard and api for each function

---
+++ frontend/src/lib/api/profile.ts
@@ -0,0 +1,12 @@
+import { API_BASE } from "."
+import { getToken } from "./auth"
+
+export async function getProfile() {
+	return fetch(`${API_BASE}/profile`, {
+		method: "GET",
+		headers: {
+            "Authorization": `Bearer ${getToken()}`,
+			"Content-Type": "application/json"
+		}
+	})
+}
