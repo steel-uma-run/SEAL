f33802d75fd87806d16587ca32059e96fe24e151
mynaux171 <thmyxuan11@gmail.com>
2026-06-09 22:35:57 +0700

    fix Track entity

--- backend/src/main/java/seal/backend/entities/Track.java
+++ backend/src/main/java/seal/backend/entities/Track.java
@@ -3,31 +3,34 @@ package seal.backend.entities;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.OffsetDateTime;
 import java.util.UUID;
+import lombok.Builder;
 import lombok.Data;
 
 @Entity
 @Table(name = "tracks")
+@Builder
 @Data
 public class Track {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
 
-  @Column(columnDefinition = "timestamptz", nullable = false)
-  @Nonnull
-  private OffsetDateTime startTime;
-
-  @Column(columnDefinition = "timestamptz", nullable = false)
-  @Nonnull
-  private OffsetDateTime endTime;
+  @Column(nullable = false, columnDefinition = "TEXT")
+  private String name;
 
   @Column(columnDefinition = "TEXT", nullable = false)
   @Nonnull
   private String description;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "season_id", nullable = false)
+  private Season season;
 }
