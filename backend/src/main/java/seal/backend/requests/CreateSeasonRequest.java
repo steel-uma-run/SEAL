327cd655c5f537c6cdeba4c467ac686c673e11ff
Scarborrower <ng.k.dien123@gmail.com>
2026-06-09 20:26:33 +0700

    Edit CreateSeasonRequest to have name

--- backend/src/main/java/seal/backend/requests/CreateSeasonRequest.java
+++ backend/src/main/java/seal/backend/requests/CreateSeasonRequest.java
@@ -4,6 +4,7 @@ import jakarta.validation.constraints.NotNull;
 import java.time.OffsetDateTime;
 
 public record CreateSeasonRequest(
+    @NotNull String name,
     @NotNull String description,
     @NotNull OffsetDateTime startTime,
     @NotNull OffsetDateTime endTime) {}
