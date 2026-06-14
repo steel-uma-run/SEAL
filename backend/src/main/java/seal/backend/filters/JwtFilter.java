9908d6122b92c627c5c1d0cc0f4f735c83b90372
ntt <nttis@duck.com>
2026-06-14 23:16:57 +0700

    filter: catch exceptions in JwtFilter

--- backend/src/main/java/seal/backend/filters/JwtFilter.java
+++ backend/src/main/java/seal/backend/filters/JwtFilter.java
@@ -6,18 +6,23 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import seal.backend.services.JwtService;
+import tools.jackson.databind.ObjectMapper;
 
 @Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
   private final UserDetailsService userDetailsService;
+  private final ObjectMapper objMapper;
 
   @Override
   protected void doFilterInternal(
@@ -30,14 +35,28 @@ public class JwtFilter extends OncePerRequestFilter {
     }
 
     String token = authorizationHeader.replace("Bearer ", "");
-    String subject = JwtService.extractEmail(token); // TODO: catch exceptions
-    UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
 
-    UsernamePasswordAuthenticationToken auth =
-        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    try {
+      String subject = JwtService.extractEmail(token);
+      UserDetails userDetails = userDetailsService.loadUserByUsername(subject);
 
-    SecurityContextHolder.getContext().setAuthentication(auth);
+      UsernamePasswordAuthenticationToken auth =
+          new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
 
-    filterChain.doFilter(request, response);
+      SecurityContextHolder.getContext().setAuthentication(auth);
+      filterChain.doFilter(request, response);
+    } catch (UsernameNotFoundException ex) {
+      ProblemDetail problemDetail =
+          ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+
+      response.setStatus(HttpStatus.BAD_REQUEST.value());
+      response.getWriter().write(objMapper.writeValueAsString(problemDetail));
+    } catch (Exception ex) {
+      ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+      logger.error(ex);
+
+      response.setStatus(HttpStatus.BAD_REQUEST.value());
+      response.getWriter().write(objMapper.writeValueAsString(problemDetail));
+    }
   }
 }
