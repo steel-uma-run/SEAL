7734a832ef2fae0e1aa86fd270e7b02b257e74b2
ntt <nttis@duck.com>
2026-06-14 23:16:57 +0700

    auth: return more info on login

--- backend/src/main/java/seal/backend/controllers/SeasonsController.java
+++ backend/src/main/java/seal/backend/controllers/SeasonsController.java
@@ -1,15 +1,20 @@
 package seal.backend.controllers;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import seal.backend.config.GlobalConfig;
 import seal.backend.services.SeasonsService;
 import seal.openapi.api.SeasonsApi;
+import seal.openapi.model.CreateSeasonRequestDto;
 import seal.openapi.model.SeasonDto;
 
 @RestController
@@ -27,5 +32,12 @@ public class SeasonsController implements SeasonsApi {
   public ResponseEntity<SeasonDto> getSeason(
       @PathVariable(name = "seasonId") @NotNull UUID seasonId) {
     return ResponseEntity.ok(seasonsService.getSeason(seasonId));
+  }
+
+  // TODO: implement
+  @Override
+  public ResponseEntity<SeasonDto> createSeason(
+      @RequestBody @Valid @NotNull CreateSeasonRequestDto body) {
+    throw new ResponseStatusException(HttpStatus.NOT_IMPLEMENTED);
   }
 }
