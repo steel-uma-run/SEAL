2390c0250b38dc91eedd2b9b1ae8b06bc3aecaa3
Scarborrower <ng.k.dien123@gmail.com>
2026-06-21 15:54:43 +0700

    Modify getAllEvents to return different data depending on roles

--- backend/src/main/java/seal/backend/services/impl/HackathonEventServiceImpl.java
+++ backend/src/main/java/seal/backend/services/impl/HackathonEventServiceImpl.java
@@ -3,6 +3,8 @@ package seal.backend.services.impl;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 import seal.backend.entities.HackathonEvent;
@@ -21,7 +23,17 @@ public class HackathonEventServiceImpl implements HackathonEventService {
 
   @Override
   public List<HackathonEvent> getAllEvents() {
-    return hackathonEventRepository.findAll();
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+    boolean isCoordinator =
+        auth != null
+            && auth.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("COORDINATOR"));
+
+    if (isCoordinator) {
+      return hackathonEventRepository.findAll();
+    }
+
+    return hackathonEventRepository.findByStatus(EventStatus.FINALIZED);
   }
 
   @Override
