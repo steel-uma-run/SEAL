fc199b2fdfdca38cebc06d66afc7cf5a9b67af93
ntt <nttis@duck.com>
2026-06-21 15:30:23 +0700

    team: get all invites API

---
+++ backend/src/main/java/seal/backend/controllers/InviteController.java
@@ -0,0 +1,27 @@
+package seal.backend.controllers;
+
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import seal.backend.config.GlobalConfig;
+import seal.backend.services.InviteService;
+import seal.openapi.api.InvitesApi;
+import seal.openapi.model.TeamInviteDto;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(GlobalConfig.API_BASE)
+public class InviteController implements InvitesApi {
+  private final InviteService inviteService;
+
+  @Override
+  @PreAuthorize("hasAuthority('STUDENT')")
+  public ResponseEntity<TeamInviteDto[]> getAllInvites() {
+    return ResponseEntity.ok(inviteService.getAllInvites().toArray(TeamInviteDto[]::new));
+  }
+}
---
+++ backend/src/main/java/seal/backend/services/impl/InviteServiceImpl.java
@@ -0,0 +1,49 @@
+package seal.backend.services.impl;
+
+import java.util.List;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import seal.backend.entities.Student;
+import seal.backend.entities.Team;
+import seal.backend.entities.TeamInvite;
+import seal.backend.enums.InviteStatus;
+import seal.backend.repositories.StudentRepository;
+import seal.backend.repositories.TeamInviteRepository;
+import seal.backend.services.InviteService;
+import seal.openapi.model.TeamInviteDto;
+import seal.openapi.model.TeamInviteStatusDto;
+
+@Service
+@RequiredArgsConstructor
+public class InviteServiceImpl implements InviteService {
+  private final TeamInviteRepository inviteRepository;
+  private final StudentRepository studentRepository;
+
+  @Override
+  public List<TeamInviteDto> getAllInvites() {
+    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+    Student actor =
+        studentRepository
+            .findByUserEmail(auth.getName())
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.FORBIDDEN));
+
+    List<TeamInvite> invites = inviteRepository.findAllByInviteeId(actor.getId());
+
+    return invites.stream()
+        .map(
+            (inviteEntity) ->
+                new TeamInviteDto(
+                    inviteEntity.getId(),
+                    inviteEntity.getSentAt(),
+                    inviteEntity.getExpiresAt(),
+                    TeamInviteStatusDto.fromValue(inviteEntity.getStatus().name()),
+                    inviteEntity.getInvitingTeam().getId()))
+        .toList();
+  }
+}
