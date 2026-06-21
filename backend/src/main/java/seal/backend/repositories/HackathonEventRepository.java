49dfd3074e3e5b33a0af99e6af885a6a4fb6a1be
Scarborrower <ng.k.dien123@gmail.com>
2026-06-21 15:54:43 +0700

    Modify getAllEvents to return different data depending on roles

--- backend/src/main/java/seal/backend/repositories/HackathonEventRepository.java
+++ backend/src/main/java/seal/backend/repositories/HackathonEventRepository.java
@@ -1,7 +1,11 @@
 package seal.backend.repositories;
 
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import seal.backend.entities.HackathonEvent;
+import seal.backend.enums.EventStatus;
 
-public interface HackathonEventRepository extends JpaRepository<HackathonEvent, UUID> {}
+public interface HackathonEventRepository extends JpaRepository<HackathonEvent, UUID> {
+  List<HackathonEvent> findByStatus(EventStatus status);
+}
