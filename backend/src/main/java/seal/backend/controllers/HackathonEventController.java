2f2b4df3c32be87c0a92d45f85c8c1e7db32d974
ntt <nttis@duck.com>
2026-06-23 18:28:28 +0700

    event: fix

--- backend/src/main/java/seal/backend/controllers/HackathonEventController.java
+++ backend/src/main/java/seal/backend/controllers/HackathonEventController.java
@@ -2,6 +2,7 @@ package seal.backend.controllers;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -9,76 +10,116 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import seal.backend.config.GlobalConfig;
 import seal.backend.services.HackathonEventService;
+import seal.backend.services.RoundService;
+import seal.backend.services.SubmissionService;
+import seal.backend.services.TrackService;
 import seal.openapi.api.EventsApi;
 import seal.openapi.model.CreateEventRequestDto;
+import seal.openapi.model.CreateRoundRequestDto;
+import seal.openapi.model.CreateTrackRequestDto;
 import seal.openapi.model.HackathonEventDto;
+import seal.openapi.model.RoundDto;
 import seal.openapi.model.StudentDto;
+import seal.openapi.model.SubmitWorkRequestDto;
+import seal.openapi.model.TeamDto;
+import seal.openapi.model.TrackDto;
 import seal.openapi.model.UpdateEventRequestDto;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping(GlobalConfig.API_BASE)
 public class HackathonEventController implements EventsApi {
   private final HackathonEventService eventService;
+  private final SubmissionService submissionService;
+  private final TrackService trackService;
+  private final RoundService roundService;
 
   @Override
   @PreAuthorize("hasAnyAuthority('COORDINATOR', 'STUDENT')")
   public ResponseEntity<StudentDto[]> getInterestedParticipants(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
       @PathVariable(name = "eventId") @NotNull UUID eventId) {
-
     return ResponseEntity.ok(
-        eventService.getInterestedParticipants(seasonId, eventId).toArray(StudentDto[]::new));
+        eventService.getInterestedParticipants(eventId).toArray(StudentDto[]::new));
   }
 
   @Override
   @PreAuthorize("hasAuthority('COORDINATOR')")
   public ResponseEntity<HackathonEventDto> updateEvent(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
       @PathVariable(name = "eventId") @NotNull UUID eventId,
-      @Valid @RequestBody UpdateEventRequestDto request) {
-
-    return ResponseEntity.ok(eventService.updateEvent(seasonId, eventId, request));
+      @Valid @RequestBody @NotNull UpdateEventRequestDto request) {
+    return ResponseEntity.ok(eventService.updateEvent(eventId, request));
   }
 
   @Override
   @PreAuthorize("hasAuthority('STUDENT')")
   public ResponseEntity<Void> markInterested(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
       @PathVariable(name = "eventId") @NotNull UUID eventId) {
-
-    eventService.markInterested(seasonId, eventId);
+    eventService.markInterested(eventId);
     return ResponseEntity.ok().build();
   }
 
   @Override
   @PreAuthorize("hasAuthority('COORDINATOR')")
-  public ResponseEntity<Void> finalizeEvent(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
-      @PathVariable(name = "eventId") @NotNull UUID eventId) {
-    eventService.finalizeEvent(seasonId, eventId);
+  public ResponseEntity<Void> finalizeEvent(@PathVariable(name = "eventId") @NotNull UUID eventId) {
+    eventService.finalizeEvent(eventId);
     return ResponseEntity.ok().build();
   }
 
   @Override
   @PreAuthorize("hasAuthority('COORDINATOR')")
   public ResponseEntity<HackathonEventDto> createEvent(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
       @RequestBody @Valid @NotNull CreateEventRequestDto request) {
     return ResponseEntity.status(HttpStatus.CREATED).body(eventService.createEvent(request));
   }
 
   @Override
-  public ResponseEntity<HackathonEventDto[]> getEventsInSeason(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId) {
-    return ResponseEntity.ok(eventService.getAllEvents(seasonId).toArray(HackathonEventDto[]::new));
+  public ResponseEntity<TeamDto[]> getAllTeamsOfEvents(
+      @PathVariable(name = "eventId") @NotNull UUID eventId) {
+    List<TeamDto> dtos = eventService.getAllTeamsOfEvent(eventId);
+    return ResponseEntity.ok(dtos.toArray(TeamDto[]::new));
   }
 
   @Override
   public ResponseEntity<HackathonEventDto> getEvent(
-      @PathVariable(name = "seasonId") @NotNull UUID seasonId,
       @PathVariable(name = "eventId") @NotNull UUID eventId) {
-    return ResponseEntity.ok(eventService.getEvent(seasonId, eventId));
+    return ResponseEntity.ok(eventService.getEvent(eventId));
+  }
+
+  @Override
+  @PreAuthorize("hasAuthority('STUDENT')")
+  public ResponseEntity<Void> submitWork(
+      @PathVariable(name = "eventId") @NotNull UUID eventId,
+      @RequestBody @Valid @NotNull SubmitWorkRequestDto body) {
+    submissionService.submitWork(eventId, body);
+    return ResponseEntity.ok().build();
+  }
+
+  @Override
+  @PreAuthorize("hasAuthority('COORDINATOR')")
+  public ResponseEntity<RoundDto> createRound(
+      @PathVariable(name = "eventId") @NotNull UUID eventId,
+      @RequestBody @Valid @NotNull CreateRoundRequestDto request) {
+    RoundDto responseDto = roundService.createRound(eventId, request);
+    return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+  }
+
+  @Override
+  @PreAuthorize("hasAuthority('COORDINATOR')")
+  public ResponseEntity<TrackDto> createTrack(
+      @PathVariable(name = "eventId") @NotNull UUID eventId,
+      @RequestBody @Valid @NotNull CreateTrackRequestDto request) {
+    TrackDto responseDto = trackService.createTrack(request, eventId);
+    return ResponseEntity.ok(responseDto);
+  }
+
+  @Override
+  public ResponseEntity<TrackDto[]> getAllTracksOfEvent(
+      @PathVariable(name = "eventId") @NotNull UUID eventId) {
+    List<TrackDto> dtos = eventService.getAllTracksOfEvent(eventId);
+    return ResponseEntity.ok(dtos.toArray(TrackDto[]::new));
   }
 }
