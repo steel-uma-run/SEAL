95dc60225adf922ccc6d13cb898289de09c18c32
ntt <nttis@duck.com>
2026-06-23 18:28:28 +0700

    fe: openapi!!!

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,36 @@
+import { defineConfig } from "vite"
 import tailwindcss from "@tailwindcss/vite"
 import { sveltekit } from "@sveltejs/kit/vite"
-import { defineConfig } from "vite"
+import { heyApiPlugin } from "@hey-api/vite-plugin"
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
-	server: { watch: { usePolling: true } }
+	plugins: [
+		heyApiPlugin({
+			config: {
+				input: "../openapi.json",
+				output: {
+					path: "src/lib/api",
+					postProcess: ["prettier"]
+				},
+				plugins: [
+					"@hey-api/typescript",
+					{
+						name: "@hey-api/client-fetch",
+						throwOnError: true
+					},
+					{
+						name: "zod",
+						dates: {
+							offset: true
+						}
+					},
+					{
+						name: "@hey-api/sdk",
+					}
+				]
+			}
+		}),
+		tailwindcss(),
+		sveltekit()
+	]
 })
