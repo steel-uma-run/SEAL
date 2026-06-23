584c00c6938d53a2ab509c8e0fc78ff6d207eea1
ntt <nttis@duck.com>
2026-06-23 18:28:28 +0700

    fix tests

--- backend/src/test/java/seal/backend/AuditLogTests.java
+++ backend/src/test/java/seal/backend/AuditLogTests.java
@@ -7,41 +7,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import seal.backend.entities.AuditLog;
-import seal.backend.entities.Coordinator;
-import seal.backend.entities.Student;
-import seal.backend.entities.User;
 import seal.backend.entities.audit.AccountApprovedLog;
 import seal.backend.entities.audit.AccountBannedLog;
-import seal.backend.enums.Role;
-import seal.backend.enums.StudentStatus;
-import seal.backend.enums.StudentType;
 import seal.backend.repositories.AuditLogRepository;
-import seal.backend.repositories.CoordinatorRepository;
-import seal.backend.repositories.StudentRepository;
-import seal.backend.repositories.UserRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class AuditLogTests {
   @Autowired private AuditLogRepository<AuditLog> repo;
-  @Autowired private UserRepository userRepo;
-  @Autowired private CoordinatorRepository coordRepo;
-  @Autowired private StudentRepository studentRepo;
-
-  Coordinator createCoord() {
-    User user = new User("NHC", Role.COORDINATOR, "", "");
-    Coordinator coord = new Coordinator(userRepo.save(user));
-
-    return coordRepo.save(coord);
-  }
-
-  Student createStudent() {
-    User user = new User("NHC", Role.COORDINATOR, "a@b.c", "");
-    Student student =
-        new Student(userRepo.save(user), StudentType.EXTERNAL, StudentStatus.ACTIVE, "S");
-
-    return studentRepo.save(student);
-  }
+  @Autowired private CreateUtils createUtils;
 
   @Test
   @Transactional
@@ -49,8 +23,8 @@ class AuditLogTests {
     repo.save(
         AccountApprovedLog.builder()
             .actionTime(OffsetDateTime.now())
-            .actor(createCoord())
-            .approvedStudent(createStudent())
+            .actor(createUtils.createCoord())
+            .approvedStudent(createUtils.createStudent())
             .build());
   }
 
@@ -60,8 +34,8 @@ class AuditLogTests {
     repo.save(
         AccountBannedLog.builder()
             .actionTime(OffsetDateTime.now())
-            .actor(createCoord())
-            .banTarget(createStudent())
+            .actor(createUtils.createCoord())
+            .banTarget(createUtils.createStudent())
             .banReason("thich")
             .build());
   }
---
+++ backend/src/test/java/seal/backend/CreateUtils.java
@@ -0,0 +1,105 @@
+package seal.backend;
+
+import java.time.OffsetDateTime;
+import java.util.Random;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import seal.backend.entities.Coordinator;
+import seal.backend.entities.HackathonEvent;
+import seal.backend.entities.Round;
+import seal.backend.entities.Season;
+import seal.backend.entities.Student;
+import seal.backend.enums.EventStatus;
+import seal.backend.enums.Role;
+import seal.backend.enums.Semester;
+import seal.backend.enums.StudentStatus;
+import seal.backend.enums.StudentType;
+import seal.backend.repositories.CoordinatorRepository;
+import seal.backend.repositories.HackathonEventRepository;
+import seal.backend.repositories.RoundRepository;
+import seal.backend.repositories.SeasonRepository;
+import seal.backend.repositories.StudentRepository;
+
+@Component
+public class CreateUtils {
+  @Autowired private CoordinatorRepository coordRepo;
+  @Autowired private StudentRepository studentRepo;
+  @Autowired private SeasonRepository seasonRepo;
+  @Autowired private HackathonEventRepository eventRepo;
+  @Autowired private RoundRepository roundRepo;
+
+  public final String randomString(int len) {
+    int leftLimit = 97; // letter 'a'
+    int rightLimit = 122; // letter 'z'
+    Random random = new Random();
+
+    String generatedString =
+        random
+            .ints(leftLimit, rightLimit + 1)
+            .limit(len)
+            .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+            .toString();
+
+    return generatedString;
+  }
+
+  public final Coordinator createCoord() {
+    Coordinator coord =
+        Coordinator.builder()
+            .email(randomString(12))
+            .fullName(randomString(12))
+            .passwordHash(randomString(32))
+            .role(Role.COORDINATOR)
+            .build();
+
+    return coordRepo.save(coord);
+  }
+
+  public final Student createStudent() {
+    Student student =
+        Student.builder()
+            .email(randomString(10))
+            .fullName(randomString(10))
+            .passwordHash(randomString(32))
+            .role(Role.STUDENT)
+            .schoolName(randomString(32))
+            .studentId(randomString(6))
+            .studentStatus(StudentStatus.PENDING)
+            .studentType(StudentType.EXTERNAL)
+            .build();
+
+    return studentRepo.save(student);
+  }
+
+  public final HackathonEvent createFinalizedEvent() {
+    Season season = new Season(Semester.SPRING, 9999);
+
+    HackathonEvent event =
+        new HackathonEvent(
+            randomString(20),
+            randomString(50),
+            OffsetDateTime.now(),
+            OffsetDateTime.now().plusDays(1),
+            EventStatus.FINALIZED,
+            season);
+
+    seasonRepo.save(season);
+    return eventRepo.save(event);
+  }
+
+  public final HackathonEvent createOngoingEvent() {
+    HackathonEvent event = createFinalizedEvent();
+
+    Round round =
+        new Round(
+            randomString(50),
+            OffsetDateTime.now(),
+            OffsetDateTime.now().plusDays(1),
+            randomString(50),
+            event);
+
+    roundRepo.save(round);
+
+    return event;
+  }
+}
