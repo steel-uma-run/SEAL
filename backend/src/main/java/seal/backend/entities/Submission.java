af41cc47abf1484dc0763d4c343abb8d8a0b1026
ntt <nttis@duck.com>
2026-06-23 18:28:28 +0700

    submission: relationships

--- backend/src/main/java/seal/backend/entities/Submission.java
+++ backend/src/main/java/seal/backend/entities/Submission.java
@@ -3,9 +3,12 @@ package seal.backend.entities;
 import jakarta.annotation.Nonnull;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -27,18 +30,33 @@ public class Submission {
   @Nonnull
   private OffsetDateTime submitTime;
 
-  @Column(columnDefinition = "TEXT", nullable = true)
+  @Column(columnDefinition = "TEXT", nullable = false)
+  @Nonnull
+  private String title;
+
+  @Column(columnDefinition = "TEXT", nullable = false)
+  @Nonnull
   private String description;
 
-  @Column(columnDefinition = "TEXT", nullable = true)
-  private String gitLink;
+  @Column(columnDefinition = "TEXT", nullable = false)
+  @Nonnull
+  private String githubLink;
 
-  @Column(columnDefinition = "TEXT", nullable = true)
+  @Column(columnDefinition = "TEXT", nullable = false)
+  @Nonnull
   private String ytLink;
 
-  @Column(columnDefinition = "TEXT", nullable = true)
+  @Column(columnDefinition = "TEXT", nullable = false)
+  @Nonnull
   private String slideLink;
 
-  @Column(columnDefinition = "TEXT", nullable = true)
-  private String title;
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "team_id", nullable = false)
+  @Nonnull
+  private Team submitterTeam;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "round_id", nullable = false)
+  @Nonnull
+  private Round round;
 }
