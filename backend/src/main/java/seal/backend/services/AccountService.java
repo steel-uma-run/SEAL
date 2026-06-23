caf24320b625f1251ec9760eb9c18371dca39f70
ntt <nttis@duck.com>
2026-06-23 18:28:28 +0700

    account: fix

--- backend/src/main/java/seal/backend/services/AccountService.java
+++ backend/src/main/java/seal/backend/services/AccountService.java
@@ -1,7 +1,12 @@
 package seal.backend.services;
 
 import java.util.UUID;
+import seal.openapi.model.StudentDto;
 
 public interface AccountService {
+  public Object[] getAllAccounts();
+
   public void approve(UUID id);
+
+  public StudentDto[] getUnapprovedStudents();
 }
