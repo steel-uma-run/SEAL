08c199d166f84852dd441780d33ff94daec0f560
ntt <nttis@duck.com>
2026-07-08 21:37:16 +0700

    move actors to inherit from User

--- backend/src/main/java/seal/backend/entities/Coordinator.java
+++ backend/src/main/java/seal/backend/entities/Coordinator.java
@@ -1,31 +1,10 @@
 package seal.backend.entities;
 
-import jakarta.annotation.Nonnull;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
-import java.util.UUID;
-import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
+import lombok.experimental.SuperBuilder;
 
 @Entity
-@Table(name = "coordinators")
 @NoArgsConstructor
-@RequiredArgsConstructor
-@Data
-public class Coordinator {
-  @Id
-  @GeneratedValue(strategy = GenerationType.UUID)
-  private UUID id;
-
-  @OneToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", nullable = false)
-  @Nonnull
-  private User user;
-}
+@SuperBuilder
+public class Coordinator extends User {}
