8d57c73678292c445f95e04d457051a7fc0110de
ntt <nttis@duck.com>
2026-07-18 17:07:42 +0700

    man

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,98 +1,127 @@
 <script lang="ts">
 	import { getAllSeasons, getEventsInSeason } from "$lib/api/sdk.gen"
-	import { formatDate, formatFullDate } from "$lib/utils/formatters.js"
 
-	import type { HackathonEvent } from "$lib/api"
-	import * as kaomojis from "$lib/kaomojis"
-	import FilledButton from "$lib/components/FilledButton.svelte"
-	import ElevatedCard from "$lib/components/ElevatedCard.svelte"
-	import TonalButton from "$lib/components/TonalButton.svelte"
+	import KaomojiError from "$lib/components/KaomojiError.svelte"
+	import EventCard from "$lib/components/EventCard.svelte"
 
-	async function getAllEvents() {
+	import "@material/web/button/filled-button"
+	import "@material/web/button/outlined-button"
+	import "@material/web/tabs/tabs"
+	import "@material/web/tabs/primary-tab"
+
+	const timeframe: "current" | "past" = $state("current")
+	const data = $derived.by(async () => {
 		const seasons = await getAllSeasons()
-		let toReturn = new Array<HackathonEvent>()
-
-		for (const season of seasons.data) {
-			const events = await getEventsInSeason({
-				path: { seasonId: season.id }
+		const events = await Promise.all(
+			seasons.data.map(async (season) => {
+				return {
+					season: season,
+					events: (await getEventsInSeason({ path: { seasonId: season.id } })).data
+				}
 			})
+		)
 
-			toReturn = toReturn.concat(events.data)
-		}
-
-		return toReturn
-	}
+		return events
+	})
 </script>
 
-<div class="mx-2 py-2">
-	<section class="flex items-center justify-center text-center min-h-screen">
-		<div class="z-2 max-w-xl">
-			<h1 class="text-6xl font-black mb-4 text-(--md-on-surface)">
-				<span class="text-(--md-primary)">SEAL</span> HACKATHON
+<div class="container">
+	<section class="hero">
+		<div>
+			<h1>
+				<span class="seal">SEAL</span> HACKATHON
 			</h1>
-			<p class="text-xl text-(--md-on-surface) opacity-80 mb-8 leading-relaxed">
+			<p>
 				The official event management and student support platform for FPT University. Register now
 				to form your team, submit your project, and experience the platform!
 			</p>
 
-			<FilledButton size="md" text={"Join now"} href="/auth/login" iconName="arrow_forward" />
+			<md-filled-button href="/auth/login">Join now</md-filled-button>
 		</div>
 	</section>
 
-	<section class="m-auto max-w-3xl mb-10">
-		<h1 class="text-3xl font-bold mb-10">Ongoing events</h1>
+	<section class="events">
+		<md-tabs>
+			<md-primary-tab>Current events</md-primary-tab>
+			<md-primary-tab>Past events</md-primary-tab>
+		</md-tabs>
 
-		<svelte:boundary>
-			{@const events = await getAllEvents()}
-
-			{#if events.length <= 0}
-				<div class="text-center flex flex-col gap-4 text-(--md-on-surface-variant)">
-					<p class="text-3xl">{kaomojis.random(kaomojis.neutral)}</p>
-					<p class="text-xl">No events are ongoing right now, check back later!</p>
+		{#await data}
+			<p style="text-align: center">Loading...</p>
+		{:then data}
+			{#if data.length <= 0}
+				<div style="text-align: center">
+					<KaomojiError
+						kind="neutral"
+						text="Seems like no events are ongoing right now. Check back later!"
+					/>
 				</div>
 			{:else}
-				{@const event = events[0]}
-				<ElevatedCard direction="left-right">
-					{#snippet header()}
-						<div class="min-h-full max-w-[40%]">
-							<img
-								class="object-cover w-full h-full rounded-tl-xl rounded-bl-xl"
-								alt="Some idk"
-								src="https://images.unsplash.com/photo-1631350397792-8e0c2de5b637?q=80&w=870&auto=format&fit=crop"
-								height="870"
-								width="580"
-							/>
-						</div>
-					{/snippet}
+				{#each data as season}
+					<div class="season">
+						<h3>{season.season.semester} {season.season.year}</h3>
 
-					<div class="flex flex-col justify-between h-full">
-						<div>
-							<h2 class="text-2xl font-bold">{event.name}</h2>
-							<p class="mb-8">{event.description}</p>
+						<div class="event-cards">
+							{#each season.events as event}
+								<EventCard {event} />
+							{/each}
 						</div>
 
-						<div>
-							<TonalButton
-								href="/events/{event.id}"
-								text="View details"
-								size="sm"
-								iconName="arrow_forward"
-							/>
-						</div>
+						<hr />
 					</div>
-				</ElevatedCard>
+				{/each}
 			{/if}
-
-			{#snippet pending()}
-				<p>Loading...</p>
-			{/snippet}
-
-			{#snippet failed()}
-				<div class="text-center flex flex-col gap-4 text-(--md-on-surface-variant)">
-					<p class="text-3xl">{kaomojis.random(kaomojis.bad)}</p>
-					<p class="text-xl">An error occured!</p>
-				</div>
-			{/snippet}
-		</svelte:boundary>
+		{:catch}
+			<div style="text-align: center">
+				<KaomojiError kind="bad" text="An error occurred!" />
+			</div>
+		{/await}
 	</section>
 </div>
+
+<style lang="scss">
+	hr {
+		color: var(--md-sys-color-outline-variant);
+	}
+
+	.container {
+		padding: 0.5rem;
+	}
+
+	.seal {
+		color: var(--md-sys-color-primary);
+	}
+
+	.hero {
+		display: flex;
+		align-items: center;
+		text-align: center;
+		min-height: 100vh;
+
+		& div {
+			margin: auto;
+			max-width: 768px;
+
+			& > h1 {
+				font-size: 2.5rem;
+				font-weight: bolder;
+				margin-bottom: 1rem;
+			}
+
+			& > p {
+				line-height: 1.5rem;
+			}
+		}
+	}
+
+	.events {
+		margin: auto;
+		max-width: 768px;
+	}
+
+	.event-cards {
+		display: grid;
+		gap: 8px;
+		grid-template-columns: repeat(3, 1fr);
+	}
+</style>
