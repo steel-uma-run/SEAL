b66a392be38e1bad75ccd7b1a3814866845f6c47
ntt <nttis@duck.com>
2026-07-24 20:51:13 +0700

    disable cors

--- backend/src/main/java/seal/backend/config/SecurityConfig.java
+++ backend/src/main/java/seal/backend/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package seal.backend.config;
 
-import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,9 +16,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import seal.backend.filters.JwtFilter;
 
 @Configuration
@@ -35,31 +31,8 @@ public class SecurityConfig {
   }
 
   @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-
-    configuration.setAllowedOrigins(
-        Arrays.asList(
-            "http://localhost:5173",
-            "http://localhost:3000",
-            "http://localhost:4173",
-            "http://localhost:5174",
-            "http://localhost:5175"));
-    configuration.setAllowedMethods(
-        Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-    configuration.setAllowedHeaders(
-        Arrays.asList("Authorization", "Content-Type", "Accept", "X-Requested-With"));
-    configuration.setAllowCredentials(true);
-
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-
-    return source;
-  }
-
-  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
+    http.cors(cors -> cors.disable())
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(
             auth ->
