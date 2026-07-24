35cfa90aafdf041f1df08b3ade98cfff5161134f
ntt <nttis@duck.com>
2026-07-24 20:51:13 +0700

    entities: create TemplatedCriteria

--- backend/src/main/java/seal/backend/repositories/CriteriaRepository.java
+++ backend/src/main/java/seal/backend/repositories/CriteriaRepository.java
@@ -3,8 +3,5 @@ package seal.backend.repositories;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import seal.backend.entities.Criteria;
-import seal.backend.entities.CriteriaTemplate;
 
-public interface CriteriaRepository extends JpaRepository<Criteria, UUID> {
-  void deleteByCriteriaTemplate(CriteriaTemplate template);
-}
+public interface CriteriaRepository extends JpaRepository<Criteria, UUID> {}
