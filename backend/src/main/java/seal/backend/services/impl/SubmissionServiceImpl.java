33ef95ea76013aee7f0feb3dfd3da3839ec68906
Scarborrower <ng.k.dien123@gmail.com>
2026-07-25 08:45:05 +0700

    Revert and modify grading

--- backend/src/main/java/seal/backend/services/impl/SubmissionServiceImpl.java
+++ backend/src/main/java/seal/backend/services/impl/SubmissionServiceImpl.java
@@ -303,7 +303,6 @@ public class SubmissionServiceImpl implements SubmissionService {
     }
 
     scoreRepo.saveAll(newScores);
-    submission.refreshAvgScore();
     submissionRepo.save(submission);
     checkScoreDeviation(submission);
   }
