9cf9c8aa6becad4b8e66de1e4be854e9737eb568
mynaux171 <thmyxuan11@gmail.com>
2026-08-01 02:48:25 +0700

    fix error font csv

--- frontend/src/routes/events/[id]/Leaderboard.svelte
+++ frontend/src/routes/events/[id]/Leaderboard.svelte
@@ -278,7 +278,8 @@
 			}
 
 			if (csvText) {
-				const blob = new Blob([csvText], { type: "text/csv;charset=utf-8;" })
+				const cleanCsvText = csvText.replace(/^\uFEFF/, "");
+                const blob = new Blob(["\uFEFF", cleanCsvText], { type: "text/csv;charset=utf-8;" })
 				const url = URL.createObjectURL(blob)
 				const link = document.createElement("a")
 				link.href = url
